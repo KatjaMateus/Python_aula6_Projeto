ad27c1820ca290319257c5d32a4264ff7eaa6897
KatjaMateus <145811394+KatjaMateus@users.noreply.github.com>
2023-11-24 17:51:01 -0300

    papel_pedra_tesoura.py

--- papel_pedra_tesoura.py
+++ papel_pedra_tesoura.py
@@ -1,38 +1,27 @@
 
-
-def resultado(c,j:str):
-    if c == "pedra":
-        if j == "pedra":
-            return "EMPATE"
-        elif j == "papel":
-            return "o jogador venceu"
-        elif j == "tesoura":
-            return "O computador venceu"
-        else:
-            return "Opção inválida. As opções são pedra, papel ou tesoura"
-    if c == "papel":
-        if j == "papel":
-            return "EMPATE"
-        elif j == "tesoura":
-            return "o jogador venceu"
-        elif j == "pedra":
-            return "O computador venceu"
-        else:
-            return "Opção inválida. As opções são pedra, papel ou tesoura"
-    if c == "tesoura":
-        if j == "tesoura":
-            return "EMPATE"
-        elif j == "pedra":
-            return "o jogador venceu"
-        elif j == "papel":
-            return "O computador venceu"
-        else:
-            return "Opção inválida. As opções são pedra, papel ou tesoura"
-
 import random
 items = ["pedra", "papel", "tesoura"]
+def resultado(c,j:str):
+    if j == c:
+        return 'EMPATE'
+    else:
+        if c == "pedra" and j == "papel" or c == "papel" and j == "tesoura" or c == "tesoura" and j == "pedra":
+            return "o jogador venceu"
+        elif c == "pedra" and j == "tesoura" or c == "papel" and j == "pedra" or c == "tesoura" and j == "papel":
+            return "O computador venceu"
+
+
 computador = (random.choice(items))
-jogador = str(input("Qual é a sua jogada? "))
+print("""OPÇÕES:
+        pedra
+        papel
+        tesoura""")
+while True:
+    jogador = str(input("Qual é a sua jogada? "))
+    if jogador not in items:
+        print("Opção inválida")
+    else:
+        break
 print(f"O jogador jogou {jogador}")
 print(f"O computador jogou {computador}")
 print(resultado(computador, jogador))
